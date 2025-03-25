e4f77aba860021e5a8413a68290d194394c3df48
itsanindyak <iankoley04@gmail.com>
2025-03-25 13:06:30 +0530

    User can change maximum time

--- src/components/typingArea.jsx
+++ src/components/typingArea.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React from "react";
+import { useTime } from "../context/timeContext";
 
-const TypingArea = ({ typingText, inpFieldValue, timeLeft, mistakes, WPM, CPM, initTyping, handleKeyDown, resetGame }) => {
-    return (
-        <div className="typing-area">
-            <div className="section1">
-                <p id="paragraph">{typingText}</p>
-            </div>
-            <div className="section2">
-                <ul className="resultDetails">
-                    <li className="time">Time Left: {timeLeft}s</li>
-                    <li className="mistakes">Mistakes: {mistakes}</li>
-                    <li className="wpm">WPM: {WPM}</li>
-                    <li className="cpm">CPM: {CPM.toFixed(2)}</li>
-                </ul>
-                <button onClick={resetGame}>Reset</button>
-            </div>
-        </div>
-    );
+const TypingArea = ({
+  typingText,
+  inpFieldValue,
+  timeLeft,
+  mistakes,
+  WPM,
+  CPM,
+  initTyping,
+  handleKeyDown,
+  resetGame,
+}) => {
+
+    const { setIsOpen } = useTime();
+    const hanldeSetButton = () => {
+      setIsOpen(true);
+    };
+  return (
+    <div className="typing-area">
+      <div className="section1">
+        <p id="paragraph">{typingText}</p>
+      </div>
+      <div className="section2">
+        <button onClick={hanldeSetButton}>Set Time</button>
+        <ul className="resultDetails">
+          <li className="time">Time Left: {timeLeft}s</li>
+          <li className="mistakes">Mistakes: {mistakes}</li>
+          <li className="wpm">WPM: {WPM}</li>
+          <li className="cpm">CPM: {CPM.toFixed(2)}</li>
+        </ul>
+        <button onClick={resetGame}>Reset</button>
+      </div>
+    </div>
+  );
 };
 
 export default TypingArea;
