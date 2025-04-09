a23dbd690f8451b7bd7ff771b4186e12576c7e61
koushik <venkatkoushik15@gmail.com>
2025-04-09 19:24:15 +0530

    .

--- backend/conn.js
+++ backend/conn.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const db =async()=>{
 try {
-    await mongoose.connect('mongodb+srv://akoushik:akoushik@cluster0.hselv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect(process.env.MONGOURI)
     console.log('connected ot db')
 } catch (error) {
     console.log('erro in connecting to db',error)
